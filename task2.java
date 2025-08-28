bdd441ca50039406777693dd550e2c846646b8aa
sinchana-14 <155062034+sinchana-14@users.noreply.github.com>
2025-08-28 10:49:50 +0530

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+        int[] marks = new int[subjects];
+        int total = 0;
+        for (int i = 0; i < subjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+            total += marks[i];
+        }
+        double average = (double) total / subjects;
+        char grade;
+        if (average >= 90) {
+            grade = 'A';
+        } else if (average >= 80) {
+            grade = 'B';
+        } else if (average >= 70) {
+            grade = 'C';
+        } else if (average >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("\n--- Result ---");
+        System.out.println("Total Marks: " + total);
+        System.out.printf("Average Percentage: %.2f%%\n", average);
+        System.out.println("Grade: " + grade);
+        sc.close();
+    }
+}
